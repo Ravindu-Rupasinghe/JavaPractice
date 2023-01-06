9146ff14291c8b7fa6277a7be636e51d3dd2805a
Ravindu-Rupasinghe <sachintharupasinghe1997@gmail.com>
2023-01-06 12:06:28 +0700

    New update for Ex5.java

--- Ex5.java
+++ Ex5.java
@@ -1,17 +1,18 @@
-//Ex4: Increasing star pattern with spaces
-/*  *
-   **
-  ***
- ****
+//Ex5: Decreasing star pattern with spaces
+/*
+****
+ ***
+  **
+   *
 */
-//Code4: 
-public class Ex4 {
+//Code5:
+public class Ex5 {
     public static void main(String args[]) {
         for (int i = 1; i < 5; i++) {
-            for (int j = 4; j >= i; j--) {
+            for (int k = 1; k <= i; k++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= i; k++) {
+            for (int j = 4; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
